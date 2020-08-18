088da367d2ccbeffbedb304a316b27d5a04a957b
litianfeng <290690260@qq.com>
2020-08-18 12:47:26 +0800

    1

---
+++ adp-algorithms/evaluation/demarchi.sh
@@ -0,0 +1,7 @@
+# Below will
+# - load relations from "scop/scop.txt",
+# - obtain connection from "pgpass_scop.conf", and
+# - prefix the output file
+export DATASET=scop	
+source evaluation_env.sh    # Load environment; make sure that $DATASET is present
+
